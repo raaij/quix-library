6670e0d5d08bf23d1fc5726ebc002275fb00eac5
Ales Saska <ales@quix.ai>
2022-03-09 10:07:16 +0100

    wip

--- applications/image-processing/webcam-input/main.js
+++ applications/image-processing/webcam-input/main.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var ejs = require('ejs');
 
 const port = 80;
 
@@ -6,10 +7,18 @@ var app = express();
 
 module.exports = app;
 
+app.set('view engine', 'ejs');
 app.use('/css', express.static('css'));
 
+console.log("ENV --- ");
+console.log(process.env);
+console.log("ENV --- ");
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.render("index", {
+        env: process.env,
+        topic_name: process.env["topic-raw"].substring( process.env.Quix__Workspace__Id.length )
+    });
 })
 
 
